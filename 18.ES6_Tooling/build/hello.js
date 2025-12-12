0017819cc022878f9b73197a2c3c6e2d6ea37492
BharathGangireddy <bharathgangireddy15@gmail.com>
2025-12-12 12:40:20 +0530

    ES6 Tooling(ES6 to ES5) using Babel

---
+++ 18.ES6_Tooling/build/hello.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/* 
+    Babel :: 
+    -->It is a transpiler that converts ES6+ code into ES5 code for better browser compatibility.
+    --> It converts advance Js and JSX into pure js
+    --> It provides advantage of using latest advanced features of JS  
+*/
+
+var c = 10;
+var k = 200;
+(function () {
+    console.log("Hello Babel");
+});
+ // here it converts let and const to var and arrow function to normal functions
+
+ // advance JS features like classes, modules, template literals, destructuring, etc.,
+  //are also transpiled to ES5 equivalents by Babel.
---
+++ 18.ES6_Tooling/index.js
@@ -0,0 +1,13 @@
+/* 
+    Babel :: 
+    -->It is a transpiler that converts ES6+ code into ES5 code for better browser compatibility.
+    --> It converts advance Js and JSX into pure js
+    --> It provides advantage of using latest advanced features of JS  
+*/
+
+
+let c = 10;
+const k = 200;
+() => {
+    console.log("Hello Babel");
+}
